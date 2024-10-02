f180a39dfcc7075e0a300548f9cb17284bd73030
GuptaMohit-01 <guptamohitlife01@gmail.com>
2024-10-02 07:24:23 +0000

    doc refer

---
+++ js_tutorial/conversion_oper.js
@@ -0,0 +1,13 @@
+// let Num = 65
+// tostr=String(Num)
+// console.log(tostr);
+// console.log(typeof tostr)
+// const b=--a
+
+// find out this is happening after converting a to string 
+// let a = "2"
+// a++;
+// console.log(2 == 1)
+// let m =String
+// to master javscript focus on objects and browser events 
+console.log("1" === "1")       
