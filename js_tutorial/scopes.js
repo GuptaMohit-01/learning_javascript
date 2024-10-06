7a52ec16d0219b949e0759f381ad3903a1a2079e
GuptaMohit-01 <guptamohitlife01@gmail.com>
2024-10-06 06:51:37 +0000

    function

---
+++ js_tutorial/scopes.js
@@ -0,0 +1,15 @@
+function one(){
+    let username ='mohit'
+    function two(){
+        let login_pass='546'
+        console.log(`your username is ${username}`)
+        console.log(`your password is ${login_pass}`)
+    }
+    two()
+    
+    // console.log(login_pass)
+
+}
+// console.log(login_pass)
+// one()
+// two()
