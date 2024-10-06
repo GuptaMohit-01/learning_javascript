b0e3caf887415e5455098fc3411c4ffbea1ef815
GuptaMohit-01 <guptamohitlife01@gmail.com>
2024-10-06 07:23:20 +0000

    this fun

---
+++ js_tutorial/thisfun.js
@@ -0,0 +1,15 @@
+// function tea(){
+//     const name = "MOHIT"
+//     console.log(this.name)
+
+// }
+// tea()
+// const chai = () => ({name:'mk'})
+    // const name = "MOHIT"
+    // console.log(this)
+
+
+// console.log(typeof chai())
+const k = ()=>({user:"mkkg"})
+console.log(k())
+console.log(typeof k(k.user))
