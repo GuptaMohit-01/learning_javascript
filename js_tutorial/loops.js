3d7bc356ffa615f66267c45e815e23b86016ba84
GuptaMohit-01 <guptamohitlife01@gmail.com>
2024-10-07 11:47:20 +0000

    new loops

--- js_tutorial/loops.js
+++ js_tutorial/loops.js
@@ -1,6 +1,17 @@
 // for loops
 for (let i = 0; i <10 ; i++) {
     const element = i;
-    console.log(element)
+    // console.log(element)
     
 } 
+for (let i = 1; i < 11; i++) {
+    // const element = array[i];
+    // console.log(i)
+    for (let j = 1; j < 11; j++) {
+        // const element = array[j];
+        // console.log(j)
+        console.log(i+'x'+ j + '='+ i*j)
+        
+    }
+    console.log("\n")
+}
