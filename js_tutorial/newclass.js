c3a4ebc0caf2c9936dcb96adb2f14127f31ae5b8
GuptaMohit-01 <guptamohitlife01@gmail.com>
2024-10-13 03:59:54 +0000

    getter and setter

--- js_tutorial/newclass.js
+++ js_tutorial/newclass.js
@@ -4,6 +4,12 @@ class user{
         this.uname=uname;
         this.password=password
     }
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password= value.toUpperCase()
+    }
 }
-const mg = new user('mohit','156')
+const mg = new user('mohit','156zgssh')
 console.log(mg.password);
